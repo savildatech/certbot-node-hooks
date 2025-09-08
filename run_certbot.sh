ea6785bfe30f72df701a8def9b1f78738a617e6d
Cameron Brown <savildatechnology@gmail.com>
2025-09-07 20:49:23 -0600

    Update run_certbot.sh

--- run_certbot.sh
+++ run_certbot.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+## for single domain:
+local domains="domain.tld"
+
+## comma-separated list for multiple domains:
+#local domains="domain.tld,sub.domain.tld"
+
+# Your email here:
+local certbot_email="your_email@mail.domain.internal"
+
+#comment out for production
+local testmode="--test-cert --dry-run"
+
 certbot certonly --manual --preferred-challenges dns \
---agree-tos -nm your_email@mail.domain.internal \
+--agree-tos -nm  "$certbot_email" \
 --manual-auth-hook /opt/certbot-node-hooks/auth.js \
 --manual-cleanup-hook /opt/certbot-node-hooks/cleanup.js \
--d domain.internal,sub.domain.internal
-
-# Add the following flags for testing
-# --test-cert --dry-run
+-d "$domains" \
+"$testmode"
