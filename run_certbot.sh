0f629fb77728b6bb01c46b47e587b00c10874a8a
Cameron Brown <savildatechnology@gmail.com>
2025-09-07 20:16:02 -0600

    Update run_certbot.sh

--- run_certbot.sh
+++ run_certbot.sh
@@ -5,3 +5,6 @@ certbot certonly --manual --preferred-challenges dns \
 --manual-auth-hook /opt/certbot-node-hooks/auth.js \
 --manual-cleanup-hook /opt/certbot-node-hooks/cleanup.js \
 -d domain.internal,sub.domain.internal
+
+# Add the following flags for testing
+# --test-cert --dry-run
